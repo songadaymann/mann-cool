d3b92e77eae62e1532e2240ff62149ca10652291
songadaymann <37905584+songadaymann@users.noreply.github.com>
2025-12-12 19:23:22 -0500

    Initial placeholder site

---
+++ README.md
@@ -0,0 +1,18 @@
+# mann.cool
+
+Placeholder site.
+
+## Develop
+
+```sh
+npm install
+npm run dev
+```
+
+## Build
+
+```sh
+npm run build
+npm run preview
+```
+
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="page">
+      <h1 className="title">mann.cool, under construction</h1>
+    </main>
+  );
+}
